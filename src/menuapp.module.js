872eaf6d73f5872310b500d0016faca18f8d73da
Sheng Cheng <charliecheng19882006@gmail.com>
2021-08-01 01:30:40 -0400

    third

--- src/menuapp.module.js
+++ src/menuapp.module.js
@@ -1,6 +1,31 @@
 (function () {
 'use strict';
 
-angular.module('MenuApp', ['data', 'ui.router']);
+angular.module('MenuApp', ['data', 'ui.router'])
+.controller('ItemsController', ItemsController)
+.controller('CategoriesController', CategoriesController)
+.component('categories', {
+  templateUrl: 'src/templates/categories.component.html',
+  bindings: {
+    items: '<'
+  }
+})
+.component('items', {
+  templateUrl: 'src/templates/items.component.html',
+  bindings: {
+    items: '<'
+  }
+});
 
+CategoriesController.$inject = ['items'];
+function CategoriesController(items) {
+  var cateCtrl = this;
+  cateCtrl.items = items;
+}
+
+ItemsController.$inject = ['items'];
+function ItemsController(items) {
+  var itemsCtrl = this;
+  itemsCtrl.items = items;
+}
 })();
